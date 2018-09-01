96dd4f2e32e6e174a90289e088dcf49e5f82b147
alsamzu <zulfalsam@gmail.com>
2018-09-01 19:44:58 +0300

    Two Dimension Array

---
+++ FirstProject/src/ExampleTwoDim.java
@@ -0,0 +1,26 @@
+
+public class ExampleTwoDim {
+
+	public static void main(String[] args) {
+		
+		int [][] TwoDim = new int [4][3];
+		int temp = 10;
+		
+		for(int i =0; i<4; i++) {
+			for(int j = 0; j<3; j++) {
+				TwoDim [i][j] = temp;
+				temp +=10;
+			}
+		}
+		
+		//Printing the values
+		
+		for(int i =0; i<4; i++) {
+			for(int j = 0; j<3; j++) {
+				System.out.print(TwoDim [i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
