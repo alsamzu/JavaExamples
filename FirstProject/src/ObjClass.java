36527e397e33351d24ef479d4bc93136d39223d8
alsamzu <zulfalsam@gmail.com>
2018-09-02 12:41:03 +0300

    Java classes and objects

---
+++ FirstProject/src/ObjClass.java
@@ -0,0 +1,15 @@
+
+public class ObjClass {
+
+		int salary;
+			int bonus;
+			 void calcPay (){
+				int totalPay = salary+bonus;
+				
+				System.out.print("Your total salary is  "+ totalPay); 
+				 
+				 
+			}
+	
+
+}
---
+++ FirstProject/src/TestEmployee.java
@@ -0,0 +1,17 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		
+	//Initializing class objects
+		 ObjClass zulfa = new ObjClass();
+		 ObjClass alsam = new ObjClass();
+		 ObjClass syombio = new ObjClass();
+	 
+		 zulfa.salary=90000;
+		 zulfa.bonus = 30000;
+		  zulfa.calcPay ();
+
+	}
+
+}
